ed5a4c041457c45f6dd485af3652bb120886255d
MihiraAthukorala <mahiathukorala2022@gmail.com>
2023-02-17 16:38:43 +0530

    NavBar

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,24 @@
+import React from "react";
+import './home.css';
 function Home() {
     return (
       <div className="App">
-        <h1>My first</h1>
-        <p>dsfsdf fdsdfsdf dfsdfsfd dfsdf</p>
+        <div className="NavWrapper">
+
+          <div className="NavContainer">
+            
+          <div className="Home">
+            Home
+          </div>
+          <div className="Contact">
+            contact
+          </div>
+          <div className="About">
+            About
+          </div>
+          </div>
+          
+        </div>
       </div>
     );
   }
